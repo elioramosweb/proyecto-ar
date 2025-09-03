7c7b8bea2391fefb96f824e3588fcd696bf5fb68
mecobi <mecobi@gmail.com>
2025-09-03 09:49:55 -0400

    segunda

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function Cube() {
   return (
     <mesh>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="red" />
+      <meshBasicMaterial color="red" />
     </mesh>
   );
 }
