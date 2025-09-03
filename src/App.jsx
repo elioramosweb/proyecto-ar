9926494c5bd4a2c831668f5e094de6087e8fb2b5
mecobi <mecobi@gmail.com>
2025-09-03 12:44:22 -0400

    con ar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import { ARCanvas, DefaultXRControllers, createXRStore } from '@react-three/xr';
 import './App.css';
 
 function Cube() {
@@ -12,9 +13,18 @@ function Cube() {
 }
 
 function App() {
+  const xrStore = createXRStore();
+
+  const handleEnterAR = () => {
+    xrStore.enterAR();
+  };
+
   return (
-    <div style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Canvas camera={{ position: [0, 0, 5] }} shadows>
+    <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <button onClick={handleEnterAR} style={{ margin: 20, padding: 10, fontSize: 18 }}>
+        Entrar en modo AR
+      </button>
+      <ARCanvas camera={{ position: [0, 0, 4], fov: 50 }} shadows style={{ width: '100vw', height: '100vh' }} store={xrStore}>
         <ambientLight intensity={0.3} />
         <directionalLight
           position={[5, 5, 5]}
@@ -23,13 +33,9 @@ function App() {
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
         />
-        {/* Plano para recibir sombra */}
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
-          <meshStandardMaterial color="#eee" />
-        </mesh>
         <Cube />
-        <OrbitControls />
-      </Canvas>
+        <DefaultXRControllers />
+      </ARCanvas>
     </div>
   );
 }
